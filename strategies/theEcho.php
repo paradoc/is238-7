c97ba8ccf37864a0019920954a2b2891c3f6fe61
Theresa <mtsagun@up.edu.ph>
2017-11-06 17:49:59 +0400

    Echo File
    This should contain the echo file

---
+++ strategies/theEcho.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace strategies;
+
+/**
+ * Class Strategy
+ * @author yourname
+ */
+abstract class Strategy
+{
+  /**
+   * @param mixed $request
+   */
+  public function __construct($request)
+  {
+    $this->request = $request;
+    $this->api_key = null;
+  }
+
+  /**
+   * undocumented function
+   *
+   * @return void
+   */
+  public function set_api_key($key)
+  {
+    $this->api_key = $key;
+  }
+
+  /**
+   * undocumented function
+   *
+   * @return void
+   */
+  public function get($request)
+  {
+    // Initiate cURL.
+    $ch = curl_init($request);
+
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, TRUE);
+
+    //Execute the request
+    $response = curl_exec($ch);
+
+    // Cleanup.
+    curl_close($ch);
+
+    return $response;
+  }
+
+  /**
+   * undocumented function
+   *
+   * @return void
+   */
+  abstract public function get_response();
+
+  /**
+   * undocumented function
+   *
+   * @return void
+   */
+  abstract protected function format_response($response);
+}
